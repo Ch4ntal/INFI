9c977d3e2a6b800e7401ee3b955b1d93e2697965
Ch4ntal <norah.schuchter@outlook.com>
2024-01-25 15:13:10 +0100

    INFI

--- src/at/schno/mario/Burg.java
+++ src/at/schno/mario/Burg.java
@@ -1,9 +1,6 @@
 package at.schno.mario;
 
 
-import at.schno.cat.Haus;
-import at.schno.cat.Katze;
-
 import java.util.ArrayList;
 import java.util.Random;
 public class Burg {
@@ -15,8 +12,7 @@ public class Burg {
         String [] TypListe = {"Toad","Luma","Koopa","Kugelwilli","Kettenhund"};
         String [] ZuhauseListe = {"Pilzkönigreich","Rosalinas Sternenwarte","Wald","unbekannt","Bowsers Festung"};
         String [] FarbeListe = {"schmimmernd","rosegoldig","schwarz","gelb","funkelnd"};
-        String [] FaehigkeitListe = {"Karate","großmaulig","rau","samtig","weich","flauschig"};
-        int[] alterListe= {3,2,1,4,5,6,10,11};
+        String [] FaehigkeitListe = {"kann Karate","mit Flügeln","mit Katzenpowerup","aus PaperMario"};
         for (int i = 1; i<100;i++) {
             //int randomNumber = random.nextInt(max + 1 - min) + min;
             int ci = r.nextInt(TypListe.length - 1 -0) + 0;
@@ -25,17 +21,17 @@ public class Burg {
             int oi = r.nextInt(FaehigkeitListe.length - 1 -0) + 0;
             mmariocharaktereListe.add(new mariocharaktere(TypListe[ci],ZuhauseListe[ni],FarbeListe[fi],FaehigkeitListe[oi]));
         }
-        mariocharaktere.setTyp("");
-        mariocharaktere.setZuhause("bösartig");
-        mariocharaktere.setFarbe("karriert");
-        mariocharaktere.setFaehigkeitliste("flauschig");
+        mariocharaktere.setTyp("Luma");
+        mariocharaktere.setZuhause("Rosalinas Sternenwarte");
+        mariocharaktere.setFarbe("rosegold");
+        mariocharaktere.setFaehigkeit("kann Karate");
 
-        mariocharaktere mariocharaktere2 = new mariocharaktere ("" "nette","weich",6,"weiße","Snow");
+        mariocharaktere mariocharaktere2 = new mariocharaktere( "Luma","Rosalinas Sternenwarte","schimmernd","kann Karate");
         mmariocharaktereListe.add(mariocharaktere2);
 
         int charakterezaehler = 1;
 
-        Haus haus = new Haus(350,"Heaststraße 24, 88732 Hamburg",8,5);
+        Haus haus = new Haus(350,"Heatstroke 24, 88732 Hamburg",8,5);
         haus.getInfo();
 
 }
--- src/at/schno/mario/mariocharaktere.java
+++ src/at/schno/mario/mariocharaktere.java
@@ -12,10 +12,71 @@ public class mariocharaktere {
     public mariocharaktere() {
     }
 
-    public mariocharaktere(String Typ, String Zuhause, String Farbe, String Fähigkeit) {
+    public mariocharaktere(String Typ, String Zuhause, String Farbe, String Faehigkeit) {
         this.Typ = Typ;
         this.Zuhause = Zuhause;
         this.Farbe = Farbe;
         this.Faehigkeit = Faehigkeit;
     }
+    public String getTyp() {
+        return Typ;
+    }
+
+    public void setTyp(String Typ) {
+        this.Typ = Typ;
+    }
+
+    public String getZuhause() {
+        return Zuhause;
+    }
+
+    public void setZuhause(String Zuhause) {
+        this.Zuhause = Zuhause;
+    }
+
+    public String getFarbe() {
+        return Farbe;
+    }
+
+    public void setFarbe(String Farbe) {
+        this.Farbe = Farbe;
+    }
+
+    public String getFaehigkeit() {
+        return Faehigkeit;
+    }
+
+    public void setFaehigkeit(String Faehigkeit) {
+        this.Faehigkeit = Faehigkeit;
+    }
+
+    public void getInfo (){
+        System.out.println("Typ des Charakters:" + this.Typ + "Zuhause: " + this.Zuhause + "Farbe: " +
+                this.Farbe + "Fähigkeit: " + this.Faehigkeit);
+    }
+}
+    public String getZuhause() {
+        return Zuhause;
+    }
+
+    public void setZuhause(String Zuhause) {
+        this.Zuhause = Zuhause;
+    }
+
+    public String getFarbe() {
+        return Farbe;
+    }
+
+    public void setFarbe1(String Farbe) {
+        this.Farbe = Farbe;
+    }
+
+    public String getFaehigkeit() {
+        return Faehigkeit;
+    }
+
+    public void setFaehigkeit(String Faehigkeit) {
+        this.Faehigkeit = Faehigkeit;
+    }
+}
 }
